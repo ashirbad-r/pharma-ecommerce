67c9abcfd47c7f4aa985763b829785049d439c1c
Ashirbad <your-email@example.com>
2026-05-17 06:19:01 +0530

    Fix vite config

--- admin-dashboard/vite.config.js
+++ admin-dashboard/vite.config.js
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5175,
-    proxy: {
-      '/api': {
-        target: 'https://pharma-backend-efh1.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api')
-      }
-    }
+    port: 3000
   }
 })
